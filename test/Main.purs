aa900eb36bea24c53a7ce7398773d63e71cbb703
Anupam Jain <ajnsit@gmail.com>
2019-11-27 14:44:03 +0530

    Add a test to verify that we don't violate parametricity
    See https://github.com/purescript-contrib/purescript-nullable/issues/7

--- test/Main.purs
+++ test/Main.purs
@@ -3,7 +3,7 @@ module Test.Main where
 import Prelude
 
 import Data.Maybe (Maybe(..))
-import Data.Nullable (toNullable, toMaybe)
+import Data.Nullable (toNullable, toMaybe, null, Nullable)
 import Effect (Effect)
 import Test.Assert (assertEqual)
 
@@ -24,4 +24,14 @@ main = do
   assertEqual
     { actual: toNullable Nothing `compare` toNullable (Just 42)
     , expected: LT
+    }
+  assertEqual
+    { actual: toMaybe (toNullable (Just 1)) == Just 1
+    , expected: true
+    }
+  -- Make sure we don't violate parametricity (See https://github.com/purescript-contrib/purescript-nullable/issues/7)
+  let (nullInt::Nullable Int) = null
+  assertEqual
+    { actual: toMaybe (toNullable (Just nullInt)) == Just nullInt
+    , expected: true
     }
